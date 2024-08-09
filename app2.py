9b8f0048185fcd32c59f834fe6f0ad7cdd5d8b20
abdullahmashhadi <abdullah6blue@gmail.com>
2024-08-09 09:18:19 +0500

    removed wsgi.py

--- app2.py
+++ app2.py
@@ -219,5 +219,5 @@ def bot_response():
 def serve_audio(filename):
     return send_from_directory('static/audio', filename)
 
-# if __name__ == '__main__':
-#     app.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
